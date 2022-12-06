989f06ebe5b0ccd657e322756262f6baffb62ec0
tomms25 <tommasobaccano@gmail.com>
2022-12-06 16:07:35 +0100

    caricamento html e js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Attraverso l'Api di Boolean generare 10 indirizzi email
+// e stamparli in pagina all’interno di una lista.
+
+
+
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        mail:[]
+      }
+    },
+    mounted:function(){
+        for(let i = 0; i <= 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(
+                (result) => {
+                    this.mail.push(result.data.response);
+                }
+            )
+        }
+    }
+  }).mount('#app')
