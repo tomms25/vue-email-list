0f34f9cb90fe1bdc43d8e34db1418432be799457
tomms25 <tommasobaccano@gmail.com>
2022-12-06 16:33:03 +0100

    esercizio base terminato

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ createApp({
       }
     },
     mounted:function(){
-        for(let i = 0; i <= 10; i++){
+        for(let i = 1; i <= 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
                     this.mail.push(result.data.response);
